87b63d2acf32df7e5966284d7dcb8f071d0b24d2
NeXaSC <carlos.augusto.cach@gmail.com>
2023-05-05 14:48:18 -0300

    segunda alt

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,15 +1,35 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelector('.slides');
+let slideIndex = 0;
 
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function() {
-    nextImage();
-}, 5000);
-
-function nextImage() {
-    count++;
-    if (count > 3) {
-        count = 1;
-    }
-    document.getElementById("radio" + count).checked = true;
+function moveSlides() {
+  const slideWidth = slider.clientWidth;
+  slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
 }
+
+function nextSlide() {
+  if (slideIndex < 2) {
+    slideIndex++;
+  } else {
+    slideIndex = 0;
+  }
+  moveSlides();
+}
+
+function prevSlide() {
+  if (slideIndex > 0) {
+    slideIndex--;
+  } else {
+    slideIndex = 2;
+  }
+  moveSlides();
+}
+
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
+
+window.addEventListener('resize', () => {
+  moveSlides();
+});
+
+setInterval(nextSlide, 5000);
